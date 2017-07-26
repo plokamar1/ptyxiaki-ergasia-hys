48e465f2b71b0062e0b218c81130fd9e1dec4067
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-07-26 18:54:48 +0300

    After posting the user data we receive the token and userId which we save in the local browser memory. after that we can redirect any new user to the main site.

--- assets/app/authentication/sign-in/sign-in.component.ts
+++ assets/app/authentication/sign-in/sign-in.component.ts
@@ -1,6 +1,9 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
+import {AuthenticationService} from "../authentication.service"
+import {User} from "../../models/user.model";
+import {Router} from "@angular/router";
 @Component({
     selector: 'app-sign-in',
     templateUrl: './sign-in.component.html'
@@ -9,26 +12,36 @@ export class SignInComponent implements OnInit{
 
     signInForm: FormGroup;
 
-/*    @Output() changeToSignUp = new EventEmitter<boolean>();
-
-    onSignUpSelected() {
-        this.changeToSignUp.emit(true);
-        console.log("Change to sign up");
-
-    }*/
+    constructor( private authService: AuthenticationService,
+                 private router: Router) {}
     ngOnInit() {
+        //onInit i declare and create the formgroup object which has the characteristics
+        //of the form elements.
         this.signInForm = new FormGroup({
             email: new FormControl(null,[
                 Validators.required,
                 Validators.pattern("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
-            ]),
+            ]),//the email is not optional thoough i has to be written according to this REGEX
             password: new FormControl(null, Validators.required)
         });
 
     }
-
-    onSubmit(form) {
-        console.log(form.form.value)
+    onSignIn(form) {
+        const user = new User(form.value.email, form.value.password);
+        this.authService.signIn(user)
+            .subscribe(
+              data => {
+                  console.log(data);
+                  //here i save the token and the userId returned from the server
+                  //to the local browser memory. This memory lasts for 2 hours
+                  localStorage.setItem('token', data.token);
+                  localStorage.setItem('userId', data.userId);
+                  this.router.navigateByUrl('/sign-in');
+              },
+              error => {
+                  console.error(error)
+              }
+            );
     }
 
 }
