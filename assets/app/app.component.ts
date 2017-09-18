23fe870c06fcf5374db1cde3a7a8b7ee430bd9e6
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-09-18 11:17:14 +0300

    Initializing the Facebook Service with the appId from facebook.

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {FacebookService} from "ngx-facebook";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
+    constructor( private fb: FacebookService,){
+
+        //fb.init should start right away. The appId and the version aren't optional
+        fb.init({appId      : '697020153828938',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v2.8'
+            }
+        );
+    }
     
 }
