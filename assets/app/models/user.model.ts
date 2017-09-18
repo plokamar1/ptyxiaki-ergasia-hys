55246ebf99166117aae479e13287f59b0b21d989
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-09-18 11:23:02 +0300

    The password isnt obligatory for the the user model because the user enters with other services also.(facebook, google etc.)

--- assets/app/models/user.model.ts
+++ assets/app/models/user.model.ts
@@ -1,7 +1,7 @@
 export class User {
     constructor( private email: string,
-                 private password: string,
+                 private password?: string,
                  private firstName?: string,
                  private lastName?: string
                  ) {}
-}
+    }
