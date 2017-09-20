6ced548b694ec4e69aa25c8fa83a289ff5f4932b
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-09-20 12:33:30 +0300

    The app checks for the login status of the user. If he is connected ( that means he accepted to authenticate with the permissions we ask) then we get some data for the user to check if he is in the database (and get a token) and if he isnt, create him and get a token as well for the local storage.

--- assets/app/authentication/authentication.service.ts
+++ assets/app/authentication/authentication.service.ts
@@ -1,18 +1,16 @@
 import {Injectable} from "@angular/core";
 import {Http, Headers, Response} from "@angular/http";
 import {Observable} from "rxjs/Observable";
-import {FacebookService, LoginResponse} from "ngx-facebook";
-
 import 'rxjs/Rx'
 
 import {User} from "../models/user.model";
 import {error} from "util";
 
+declare const FB: any;
 @Injectable()
 export class AuthenticationService {
 
-    constructor( private http: Http,
-                 private fb: FacebookService,) {}
+    constructor( private http: Http) {}
 
     signUp(user: User){
         const body = JSON.stringify(user);
@@ -30,25 +28,33 @@ export class AuthenticationService {
     }
 
     FBSignIn(){
-        const options  = {
-            scope: 'public_profile,user_friends,email,pages_show_list',
-            return_scopes: true,
-            enable_profile_selector: true
-        };
 
-        this.fb.getLoginStatus()
-            .then((res) => {
-            console.log(res);
-            });
-
-        this.fb.login(options)
-            .then(
-                (res: LoginResponse ) => {return res;}
-            )
-            .catch(
-                (error: LoginResponse) =>{console.log(error);}
-            )
-
-
+        //check login status. if connected then ask for user email. The user will be matched in the database by email
+        //and user fb id
+        FB.getLoginStatus(function(response) {
+            if( response.status === "connected"){
+                //get the email of the user from facebook
+                FB.api('/me','get',{fields:"email"},function (resp) {
+                    console.log(response);
+                    const user = new User(resp.email,'FB','','', response.authResponse.userID);
+                    return user;
+                });
+            }else{
+                //if the user isnt connected a pop up window appears and asks for authentication
+                FB.login(function(response) {
+                    if(response.status === "connected"){
+                        //then the same happens and we produce the user model which will be sent to the database
+                        FB.api('/me','get',{fields:"email"},function (resp) {
+                            console.log(response);
+                            const user = new User(resp.email,'FB','','', response.authResponse.userID);
+                            console.log(user);
+                            return user;
+                        });
+                    }
+                }, //these are the permissions we are asking the user to give us
+                    {scope: 'public_profile,user_friends,email,pages_show_list', return_scopes: true});
+            }
+        });
     }
+
 }
