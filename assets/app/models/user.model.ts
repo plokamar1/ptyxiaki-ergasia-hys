af0943850a0777a78a3fee4262794e052ba16228
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-09-20 12:34:28 +0300

    The model changed and we ask for a loginType so the node will do the checks according to the type of login.

--- assets/app/models/user.model.ts
+++ assets/app/models/user.model.ts
@@ -1,7 +1,9 @@
 export class User {
     constructor( private email: string,
+                 private loginType: string,
                  private password?: string,
                  private firstName?: string,
-                 private lastName?: string
-                 ) {}
+                 private lastName?: string,
+                 private userId?: string
+                ) {}
     }
